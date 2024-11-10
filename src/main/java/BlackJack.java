1fd6d6e1b360482e70bea48b02c9f8f8a1651ddc
softdeveloper23 <brannongarrett77@gmail.com>
2024-11-10 13:29:48 +0100

    Created a Card class with values and constructor

--- src/main/java/BlackJack.java
+++ src/main/java/BlackJack.java
@@ -5,6 +5,16 @@ import java.util.Random;
 import javax.swing.*;
 
 public class BlackJack {
+    private class Card {
+        String value;
+        String type;
+
+        Card(String value, String type) {
+            this.value = value;
+            this.type = type;
+        }
+    }
+
     BlackJack() {
         startGame();
     }
