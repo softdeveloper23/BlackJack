e97a1cfc49c1f70da81037b359b15b26ebbb1492
softdeveloper23 <brannongarrett77@gmail.com>
2024-11-14 13:10:40 +0100

    Added code for player in startGame function

--- src/main/java/blackjack/BlackJack.java
+++ src/main/java/blackjack/BlackJack.java
@@ -77,6 +77,18 @@ public class BlackJack {
         System.out.println(dealerHand);
         System.out.println(dealerSum);
         System.out.println(dealerAceCount);
+
+        // Player.
+        playerHand = new ArrayList<Card>();
+        playerSum = 0;
+        playerAceCount = 0;
+
+        for (int i = 0; i < 2; i++) {
+            card = deck.remove(deck.size() - 1);
+            playerSum += card.getValue();
+            playerAceCount += card.isAce() ? 1 : 0;
+            playerHand.add(card);
+        }
     }
 
     public void buildDeck() {
